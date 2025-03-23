405d02ca840d1df3896f1eb7d7f34520ddae729e
Min-Uk.Lee <minuk.dev@gmail.com>
2025-03-23 18:47:51 +0900

    feat: Support connection

---
+++ pkg/utils/clock/clock.go
@@ -0,0 +1,31 @@
+package clock
+
+import (
+	"time"
+
+	k8sclock "k8s.io/utils/clock"
+)
+
+// Clock is from k8s.io/utils/clock.
+// utils/clock package is a adapter class for k8s.io/utils/clock.
+type Clock interface {
+	PassiveClock
+	// After returns the channel of a new Timer.
+	// This method does not allow to free/GC the backing timer before it fires. Use
+	// NewTimer instead.
+	After(d time.Duration) <-chan time.Time
+	// NewTimer returns a new Timer.
+	NewTimer(d time.Duration) k8sclock.Timer
+	// Sleep sleeps for the provided duration d.
+	// Consider making the sleep interruptible by using 'select' on a context channel and a timer channel.
+	Sleep(d time.Duration)
+	// Tick returns the channel of a new Ticker.
+	// This method does not allow to free/GC the backing ticker. Use
+	// NewTicker from WithTicker instead.
+	Tick(d time.Duration) <-chan time.Time
+}
+
+type PassiveClock interface {
+	Now() time.Time
+	Since(at time.Time) time.Duration
+}
