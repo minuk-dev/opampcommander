7786343ca63c52f50d75fc12d2d390b2a0a18850
Min Uk Lee <minuk.dev@gmail.com>
2025-05-02 17:23:33 +0900

    fix: Fix conn error (#29)
    * fix: Fix conn error
    
    * chore: Remove comment & rename identify

--- pkg/app/http.go
+++ pkg/app/http.go
@@ -25,15 +25,16 @@ func NewHTTPServer(
 	engine *gin.Engine,
 	settings *ServerSettings,
 	logger *slog.Logger,
+	connContext func(context.Context, net.Conn) context.Context,
 ) *http.Server {
 	//exhaustruct:ignore
 	srv := &http.Server{
 		ReadTimeout: DefaultHTTPReadTimeout,
 		Addr:        settings.Addr,
 		Handler:     engine,
+		ConnContext: connContext,
 	}
 
-	// srv.ConnContext = opampController.ConnContext
 	lifecycle.Append(fx.Hook{
 		OnStart: func(context.Context) error {
 			listener, err := net.Listen("tcp", srv.Addr)
--- pkg/app/server.go
+++ pkg/app/server.go
@@ -1,6 +1,8 @@
 package app
 
 import (
+	"context"
+	"net"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -41,14 +43,19 @@ func NewServer(settings ServerSettings) *Server {
 			fx.Annotate(NewEngine, fx.ParamTags(`group:"controllers"`)),
 			NewLogger,
 		),
-
 		// controllers
 		fx.Provide(
-			AsController(ping.NewController),
-			AsController(opamp.NewController),
-			AsController(connection.NewController),
-			AsController(agent.NewController),
-			AsController(command.NewController),
+			ping.NewController, AsController(Identity[*ping.Controller]),
+			opamp.NewController, AsController(Identity[*opamp.Controller]),
+			connection.NewController, AsController(Identity[*connection.Controller]),
+			agent.NewController, AsController(Identity[*agent.Controller]),
+			command.NewController, AsController(Identity[*command.Controller]),
+		),
+		// opamp spec
+		fx.Provide(
+			func(opampController *opamp.Controller) func(context.Context, net.Conn) context.Context {
+				return opampController.ConnContext
+			},
 		),
 		// application
 		fx.Provide(
@@ -94,4 +101,13 @@ func AsController(f any) any {
 // Controller is an interface that defines the methods for handling HTTP requests.
 type Controller interface {
 	RoutesInfo() gin.RoutesInfo
+}
+
+// Identity is a generic function that returns the input value.
+// It is a helper function to generate a function that returns the input value.
+// It is used to provide a function as a interface.
+//
+//nolint:ireturn
+func Identity[T any](a T) T {
+	return a
 }
