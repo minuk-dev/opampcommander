1cb177db12ff0e8a7eb27b1db00e2c79f767407d
Min-Uk.Lee <minuk.dev@gmail.com>
2025-05-04 16:57:20 +0900

    test: Add testutil & controller test

---
+++ internal/adapter/in/http/v1/ping/controller_test.go
@@ -0,0 +1,32 @@
+package ping_test
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/minuk-dev/opampcommander/internal/adapter/in/http/v1/ping"
+	"github.com/minuk-dev/opampcommander/pkg/testutil"
+)
+
+func TestPingController_Handle(t *testing.T) {
+	t.Parallel()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	cancel()
+
+	base := testutil.NewControllerBase()
+	base.SetupRouter(ping.NewController(base.Logger))
+	router := base.Router
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequestWithContext(ctx, http.MethodGet, "/api/v1/ping", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.JSONEq(t, "{\"message\":\"pong\"}", w.Body.String())
+}
