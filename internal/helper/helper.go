b48deb4b54afd4e47718f560c07200fac13206c8
Min-Uk.Lee <minuk.dev@gmail.com>
2025-05-17 02:58:42 +0900

    fix: fix runtime error

--- internal/helper/helper.go
+++ internal/helper/helper.go
@@ -5,6 +5,10 @@ import "context"
 
 // Runner is an interface that defines a runner that can be started with a context.
 type Runner interface {
+	// Name returns the name of the runner.
+	// It's only for debugging & logging purposes.
+	Name() string
+
 	// Run starts the runner with the given context.
 	Run(ctx context.Context) error
 }
---
+++ pkg/app/executor.go
@@ -0,0 +1,58 @@
+package app
+
+import (
+	"context"
+	"log/slog"
+	"sync"
+
+	"go.uber.org/fx"
+
+	"github.com/minuk-dev/opampcommander/internal/helper"
+)
+
+// Executor is a struct that schedules and manages the execution of runners.
+// It uses a WaitGroup to wait for all runners to finish before stopping.
+type Executor struct {
+	wg sync.WaitGroup
+}
+
+// NewExecutor creates a new Executor instance.
+func NewExecutor(
+	lifecycle fx.Lifecycle,
+	runners []helper.Runner,
+	logger *slog.Logger,
+) *Executor {
+	executor := &Executor{
+		wg: sync.WaitGroup{},
+	}
+	executorCtx, cancel := context.WithCancel(context.Background())
+
+	lifecycle.Append(fx.Hook{
+		OnStart: func(context.Context) error {
+			for _, runner := range runners {
+				executor.wg.Add(1)
+				go func(r helper.Runner) {
+					defer executor.wg.Done()
+					err := r.Run(executorCtx)
+					if err != nil {
+						logger.Error("Runner error",
+							slog.String("runner", r.Name()),
+							slog.String("error", err.Error()),
+						)
+					}
+				}(runner)
+			}
+
+			return nil
+		},
+		OnStop: func(context.Context) error {
+			cancel()
+			// Wait for all runners to finish
+			executor.wg.Wait()
+
+			return nil
+		},
+	})
+
+	return executor
+}
