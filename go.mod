1a34e7fde29921017d8269e5f2302f62afe3b4a8
Min-Uk.Lee <minuk.dev@gmail.com>
2025-05-26 02:58:32 +0900

    feat(wip): support config file

--- go.mod
+++ go.mod
@@ -6,31 +6,35 @@ require (
 	github.com/gin-gonic/gin v1.10.0
 	github.com/go-resty/resty/v2 v2.16.5
 	github.com/google/uuid v1.6.0
-	github.com/markbates/goth v1.81.0
 	github.com/open-telemetry/opamp-go v0.19.0
 	github.com/samber/lo v1.49.1
 	github.com/samber/slog-gin v1.15.1
 	github.com/spf13/cobra v1.8.1
+	github.com/spf13/viper v1.20.1
 	github.com/stretchr/testify v1.10.0
 	go.etcd.io/etcd/client/v3 v3.5.18
 	go.uber.org/fx v1.23.0
 	go.uber.org/goleak v1.2.0
+	golang.org/x/oauth2 v0.26.0
 	gopkg.in/yaml.v3 v3.0.1
 	gotest.tools v2.2.0+incompatible
 	k8s.io/utils v0.0.0-20250321185631-1f6e0b77f77e
 )
 
 require (
-	github.com/go-chi/chi/v5 v5.1.0 // indirect
-	github.com/gorilla/context v1.1.1 // indirect
-	github.com/gorilla/mux v1.6.2 // indirect
-	github.com/gorilla/securecookie v1.1.1 // indirect
-	github.com/gorilla/sessions v1.1.1 // indirect
+	github.com/fsnotify/fsnotify v1.8.0 // indirect
+	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
+	github.com/rogpeppe/go-internal v1.12.0 // indirect
+	github.com/sagikazarmark/locafero v0.7.0 // indirect
+	github.com/sourcegraph/conc v0.3.0 // indirect
+	github.com/spf13/afero v1.12.0 // indirect
+	github.com/spf13/cast v1.7.1 // indirect
+	github.com/subosito/gotenv v1.6.0 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.0 // indirect
 	go.opentelemetry.io/otel v1.35.0 // indirect
 	go.opentelemetry.io/otel/trace v1.35.0 // indirect
-	golang.org/x/oauth2 v0.26.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 )
 
 require (
@@ -58,10 +62,10 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
-	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/pflag v1.0.6 // indirect
 	github.com/stretchr/objx v0.5.2 // indirect
 	github.com/tidwall/gjson v1.18.0
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
--- pkg/cmd/apiserver/apiserver.go
+++ pkg/cmd/apiserver/apiserver.go
@@ -4,19 +4,37 @@ package apiserver
 import (
 	"fmt"
 	"log/slog"
+	"os"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 
 	"github.com/minuk-dev/opampcommander/pkg/app"
 )
 
 // CommandOption contains the options for the apiserver command.
 type CommandOption struct {
+	configFilename string
+
 	// flags
-	dbHost    string
+	dbType    string
+	dbHosts   []string
 	addr      string
 	logLevel  string
 	logFormat string
+	// flags auth
+	authEnabled              bool
+	authType                 string
+	authOauth2Provider       string
+	authOauth2ClientID       string
+	authOauth2ClientSecret   string
+	authOauth2RedirectURL    string
+	authOauth2StateMode      string
+	authOauth2StateJWTSecret string
+
+	// viper
+	viper *viper.Viper
 
 	// internal
 	app *app.Server
@@ -28,6 +46,13 @@ func NewCommand(opt CommandOption) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "apiserver",
 		Short: "apiserver",
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			err := opt.Init(cmd, args)
+			if err != nil {
+				return fmt.Errorf("failed to initialize command: %w", err)
+			}
+			return nil
+		},
 		RunE: func(cmd *cobra.Command, args []string) error {
 			err := opt.Prepare(cmd, args)
 			if err != nil {
@@ -43,12 +68,48 @@ func NewCommand(opt CommandOption) *cobra.Command {
 		},
 	}
 
-	cmd.Flags().StringVar(&opt.addr, "addr", ":8080", "server address")
-	cmd.Flags().StringVar(&opt.dbHost, "db-host", "localhost:2379", "etcd host")
-	cmd.Flags().StringVar(&opt.logLevel, "log-level", "info", "log level (debug, info, warn, error)")
-	cmd.Flags().StringVar(&opt.logFormat, "log-format", "json", "log format (json, text)")
+	cmd.PersistentFlags().StringVar(&opt.configFilename, "config", "", "config file (default is $HOME/.config/opampcommander/apiserver/config.yaml)")
+	cmd.Flags().StringVar(&opt.addr, "address", ":8080", "server address")
+	cmd.Flags().StringVar(&opt.dbType, "database.type", "etcd", "etcd")
+	cmd.Flags().StringSliceVar(&opt.dbHosts, "database.endpoints", []string{"localhost:2379"}, "etcd host")
+	cmd.Flags().StringVar(&opt.logLevel, "log.level", "info", "log level (debug, info, warn, error)")
+	cmd.Flags().StringVar(&opt.logFormat, "log.format", "json", "log format (json, text)")
+	cmd.Flags().BoolVar(&opt.authEnabled, "auth.enabled", false, "enable authentication")
+	cmd.Flags().StringVar(&opt.authType, "auth.type", "oauth2", "authentication type")
+	cmd.Flags().StringVar(&opt.authOauth2Provider, "auth.oauth2.provider", "", "OAuth2 provider URL")
+	cmd.Flags().StringVar(&opt.authOauth2ClientID, "auth.oauth2.clientId", "", "OAuth2 client ID")
+	cmd.Flags().StringVar(&opt.authOauth2ClientSecret, "auth.oauth2.clientSecret", "", "OAuth2 client secret")
+	cmd.Flags().StringVar(&opt.authOauth2RedirectURL, "auth.oauth2.redirectUri", "", "OAuth2 redirect URL")
+	cmd.Flags().StringVar(&opt.authOauth2StateMode, "auth.oauth2.state.mode", "jwt", "OAuth2 state mode (jwt)")
+	cmd.Flags().StringVar(&opt.authOauth2StateJWTSecret, "auth.oauth2.state.jwt.secret", "", "OAuth2 state JWT secret")
+	opt.viper.BindPFlags(cmd.Flags())
 
 	return cmd
+}
+
+func (opt *CommandOption) Init(_ *cobra.Command, _ []string) error {
+	if opt.configFilename != "" {
+		viper.SetConfigFile(opt.configFilename)
+	} else {
+		home, err := os.UserHomeDir()
+		if err != nil {
+			return fmt.Errorf("failed to get user home directory: %w", err)
+		}
+
+		opt.viper.AddConfigPath(filepath.Join(home, ".config", "opampcommander", "apiserver"))
+		opt.viper.SetConfigName("config")
+		opt.viper.SetConfigType("yaml")
+	}
+
+	opt.viper.AutomaticEnv() // read in environment variables that match
+
+	if err := opt.viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			return fmt.Errorf("failed to read config file: %w", err)
+		}
+	}
+
+	return nil
 }
 
 // Prepare prepares the command.
@@ -56,7 +117,7 @@ func (opt *CommandOption) Prepare(_ *cobra.Command, _ []string) error {
 	logLevel := toSlogLevel(opt.logLevel)
 	opt.app = app.NewServer(app.ServerSettings{
 		Addr:      opt.addr,
-		EtcdHosts: []string{opt.dbHost},
+		EtcdHosts: opt.dbHosts,
 		LogLevel:  logLevel,
 		LogFormat: app.LogFormat(opt.logFormat),
 	})
