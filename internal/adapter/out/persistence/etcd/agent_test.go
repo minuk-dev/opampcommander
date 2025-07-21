1c98644986d6ffe07d28c6ed092cf77ba27a8972
Min-Uk.Lee <minuk.dev@gmail.com>
2025-07-21 16:19:45 +0000

    feat(etcd): add testcontainers support for agent tests and improve error handling

--- internal/adapter/out/persistence/etcd/agent_test.go
+++ internal/adapter/out/persistence/etcd/agent_test.go
@@ -4,15 +4,21 @@ import (
 	"testing"
 
 	"github.com/google/uuid"
-	"github.com/minuk-dev/opampcommander/internal/adapter/out/persistence/etcd"
-	"github.com/minuk-dev/opampcommander/pkg/testutil"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"github.com/testcontainers/testcontainers-go"
 	etcdTestContainer "github.com/testcontainers/testcontainers-go/modules/etcd"
 	clientv3 "go.etcd.io/etcd/client/v3"
+
+	"github.com/minuk-dev/opampcommander/internal/adapter/out/persistence/etcd"
+	"github.com/minuk-dev/opampcommander/internal/domain/model"
+	"github.com/minuk-dev/opampcommander/internal/domain/model/remoteconfig"
+	domainport "github.com/minuk-dev/opampcommander/internal/domain/port"
+	"github.com/minuk-dev/opampcommander/pkg/testutil"
 )
 
 func TestAgentEtcdAdapter_GetAgent(t *testing.T) {
+	testcontainers.SkipIfProviderIsNotHealthy(t)
 	t.Parallel()
 	base := testutil.NewBase(t)
 	ctx := t.Context()
@@ -22,6 +28,7 @@ func TestAgentEtcdAdapter_GetAgent(t *testing.T) {
 	etcdEndpoint, err := etcdContainer.ClientEndpoint(ctx)
 	require.NoError(t, err)
 
+	//exhaustruct:ignore
 	etcdClient, err := clientv3.New(clientv3.Config{
 		Endpoints: []string{etcdEndpoint},
 	})
@@ -57,55 +64,120 @@ func TestAgentEtcdAdapter_GetAgent(t *testing.T) {
 		agent, err := agentEtcdAdapter.GetAgent(ctx, instanceUID)
 
 		// then
-		assert.NoError(t, err)
+		require.NoError(t, err)
 		assert.Equal(t, instanceUID, agent.InstanceUID)
 	})
 
 	t.Run("Agent not found", func(t *testing.T) {
 		t.Parallel()
-		// Test case for when the agent is not found in etcd
-	})
 
-	t.Run("Multiple agents found", func(t *testing.T) {
-		t.Parallel()
-		// Test case for when multiple agents are found in etcd
-	})
-
-	t.Run("Etcd error", func(t *testing.T) {
-		t.Parallel()
-		// Test case for when there is an error communicating with etcd
+		notExistUID := uuid.New()
+		// when
+		agent, err := agentEtcdAdapter.GetAgent(ctx, notExistUID)
+		// then
+		require.ErrorIs(t, err, domainport.ErrAgentNotExist)
+		assert.Nil(t, agent)
 	})
 }
 
 func TestAgentEtcdAdapter_ListAgents(t *testing.T) {
+	testcontainers.SkipIfProviderIsNotHealthy(t)
 	t.Parallel()
-	ctx := t.Context()
-	etcdContainer, err := etcdTestContainer.Run(ctx, "gcr.io/etcd-development/etcd:v3.5.14")
-	require.NoError(t, err)
-
-	etcdEndpoint, err := etcdContainer.ClientEndpoint(ctx)
-	require.NoError(t, err)
-
-	etcdClient, err := clientv3.New(clientv3.Config{
-		Endpoints: []string{etcdEndpoint},
-	})
-	require.NoError(t, err)
-	t.Cleanup(func() {
-		err := etcdClient.Close()
-		require.NoError(t, err)
-	})
 
 	t.Run("Happy case", func(t *testing.T) {
 		t.Parallel()
-	})
+		ctx := t.Context()
+		etcdContainer, err := etcdTestContainer.Run(ctx, "gcr.io/etcd-development/etcd:v3.5.14")
+		require.NoError(t, err)
 
-	t.Run("No agents found", func(t *testing.T) {
-		t.Parallel()
-		// Test case for when no agents are found in etcd
-	})
+		etcdEndpoint, err := etcdContainer.ClientEndpoint(ctx)
+		require.NoError(t, err)
 
-	t.Run("Etcd error", func(t *testing.T) {
+		//exhaustruct:ignore
+		etcdClient, err := clientv3.New(clientv3.Config{
+			Endpoints: []string{etcdEndpoint},
+		})
+		require.NoError(t, err)
+		t.Cleanup(func() {
+			err := etcdClient.Close()
+			require.NoError(t, err)
+		})
+
+		instanceUID := uuid.New()
+		_, err = etcdClient.Put(ctx, "agents/"+instanceUID.String(), `
+		{
+			"instanceUID": "`+instanceUID.String()+`",
+			"capabilities": null,
+			"description": null,
+			"effectiveConfig": null,
+			"packageStatuses": null,
+			"componentHealth": null,
+			"remoteConfig": {},
+			"customCapabilities": null,
+			"availableComponents": null,
+			"reportFullState": false
+		}`)
+		require.NoError(t, err)
+
+		agentEtcdAdapter := etcd.NewAgentEtcdAdapter(etcdClient, testutil.NewBase(t).Logger)
+
+		// when
+		listResponse, err := agentEtcdAdapter.ListAgents(ctx, nil)
+
+		// then
+		require.NoError(t, err)
+		assert.NotNil(t, listResponse)
+		// at least one agent should be present
+		// because for better test performance, we uses shared etcd instance
+		assert.Greater(t, listResponse.Items, 1)
+	})
+}
+
+func TestAgentEtcdAdapter_PutAgent(t *testing.T) {
+	testcontainers.SkipIfProviderIsNotHealthy(t)
+	t.Parallel()
+
+	t.Run("Happy case", func(t *testing.T) {
 		t.Parallel()
-		// Test case for when there is an error communicating with etcd
+		ctx := t.Context()
+		etcdContainer, err := etcdTestContainer.Run(ctx, "gcr.io/etcd-development/etcd:v3.5.14")
+		require.NoError(t, err)
+
+		etcdEndpoint, err := etcdContainer.ClientEndpoint(ctx)
+		require.NoError(t, err)
+
+		//exhaustruct:ignore
+		etcdClient, err := clientv3.New(clientv3.Config{
+			Endpoints: []string{etcdEndpoint},
+		})
+		require.NoError(t, err)
+		t.Cleanup(func() {
+			err := etcdClient.Close()
+			require.NoError(t, err)
+		})
+
+		instanceUID := uuid.New()
+		agentEtcdAdapter := etcd.NewAgentEtcdAdapter(etcdClient, testutil.NewBase(t).Logger)
+
+		// when
+		err = agentEtcdAdapter.PutAgent(ctx, &model.Agent{
+			InstanceUID:         instanceUID,
+			Capabilities:        nil,
+			Description:         nil,
+			EffectiveConfig:     nil,
+			PackageStatuses:     nil,
+			ComponentHealth:     nil,
+			RemoteConfig:        remoteconfig.New(),
+			CustomCapabilities:  nil,
+			AvailableComponents: nil,
+			ReportFullState:     false,
+		})
+		require.NoError(t, err)
+
+		// then
+		getResponse, err := etcdClient.Get(ctx, "agents/"+instanceUID.String())
+		require.NoError(t, err)
+		assert.Equal(t, 1, getResponse.Count)
+		assert.NotEmpty(t, getResponse.Kvs[0].Value)
 	})
 }
