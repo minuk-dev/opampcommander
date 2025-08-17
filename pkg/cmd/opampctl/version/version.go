e4371a53e9b0e2fe90ab026050b5e59b8bbf42b5
Min-Uk.Lee <minuk.dev@gmail.com>
2025-08-17 17:47:39 +0900

    fix(version): prevent nil pointer dereference for server version

--- pkg/cmd/opampctl/version/version.go
+++ pkg/cmd/opampctl/version/version.go
@@ -82,6 +82,7 @@ func (opt *CommandOptions) Run(cmd *cobra.Command, _ []string) error {
 		versionInfo.ServerVersion, serverErr = opt.client.GetServerVersion(cmd.Context())
 		if serverErr != nil {
 			cmd.PrintErrf("Failed to get server version: %v\n", serverErr)
+			versionInfo.ServerVersion = &v1version.Info{} // to prevent nil pointer dereference
 		}
 	}
 
@@ -125,4 +126,12 @@ func (opt *CommandOptions) Run(cmd *cobra.Command, _ []string) error {
 	}
 
 	return nil
+}
+
+func switchIfNil[T any](ptr *T, defaultValue T) T {
+	if ptr == nil {
+		return defaultValue
+	}
+
+	return *ptr
 }
