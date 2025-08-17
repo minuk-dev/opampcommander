d0fd1a2bbef180602b5daf15bc8df78065d19701
Min-Uk.Lee <minuk.dev@gmail.com>
2025-08-17 17:43:02 +0900

    refactor: remove dup

--- pkg/formatter/formatter.go
+++ pkg/formatter/formatter.go
@@ -81,87 +81,18 @@ func FormatJSON(writer io.Writer, target any) error {
 
 // FormatText formats the target as a text representation.
 // It uses the field name as the key and the field value as the value.
-//
-//nolint:varnamelen,err113,intrange,errcheck,gosec,mnd,funlen,exhaustive
 func FormatText(w io.Writer, data any) error {
-	v := reflect.ValueOf(data)
-
-	var slice reflect.Value
-
-	switch v.Kind() {
-	case reflect.Slice:
-		if v.Len() == 0 {
-			return errors.New("no data to display")
-		}
-
-		slice = v
-	case reflect.Struct, reflect.Ptr:
-		slice = reflect.MakeSlice(reflect.SliceOf(v.Type()), 1, 1)
-		slice.Index(0).Set(v)
-	default:
-		return errors.New("data must be a slice or struct")
-	}
-
-	firstElem := slice.Index(0)
-	if firstElem.Kind() == reflect.Ptr {
-		firstElem = firstElem.Elem()
-	}
-
-	elemType := firstElem.Type()
-
-	var (
-		fieldIndexes []int
-		fieldNames   []string
-	)
-
-	for i := 0; i < elemType.NumField(); i++ {
-		field := elemType.Field(i)
-		if field.Name == "Name" || field.Name == "ID" || field.Tag.Get("text") != "" {
-			fieldIndexes = append(fieldIndexes, i)
-			fieldNames = append(fieldNames, field.Name)
-		}
-	}
-
-	tw := tabwriter.NewWriter(w, 0, 0, 2, ' ', 0)
-
-	// Header
-	for _, name := range fieldNames {
-		fmt.Fprintf(tw, "%s\t", name)
-	}
-
-	fmt.Fprintln(tw)
-
-	// Separator
-	for range fieldNames {
-		fmt.Fprintf(tw, "--------\t")
-	}
-
-	fmt.Fprintln(tw)
-
-	// Rows
-	for i := 0; i < slice.Len(); i++ {
-		row := slice.Index(i)
-		if row.Kind() == reflect.Ptr {
-			row = row.Elem()
-		}
-
-		for _, idx := range fieldIndexes {
-			fmt.Fprintf(tw, "%v\t", row.Field(idx).Interface())
-		}
-
-		fmt.Fprintln(tw)
-	}
-
-	tw.Flush()
-
-	return nil
+	return formatCustomTags(w, data, "text")
 }
 
 // FormatShort formats the target struct to a short text representation.
 // If the field has a "short" tag, it uses the value of that tag as the key.
-//
-//nolint:errcheck,varnamelen,mnd,intrange,gosec,funlen,err113,exhaustive
 func FormatShort(w io.Writer, data any) error {
+	return formatCustomTags(w, data, "short")
+}
+
+//nolint:errcheck,varnamelen,mnd,intrange,gosec,funlen,err113,exhaustive
+func formatCustomTags(w io.Writer, data any, tag string) error {
 	v := reflect.ValueOf(data)
 
 	var slice reflect.Value
@@ -194,7 +125,7 @@ func FormatShort(w io.Writer, data any) error {
 
 	for i := 0; i < elemType.NumField(); i++ {
 		field := elemType.Field(i)
-		if field.Name == "Name" || field.Name == "ID" || field.Tag.Get("short") != "" {
+		if field.Name == "Name" || field.Name == "ID" || field.Tag.Get(tag) != "" {
 			fieldIndexes = append(fieldIndexes, i)
 			fieldNames = append(fieldNames, field.Name)
 		}
