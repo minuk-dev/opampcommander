8eb39970003821e0ac5ee85418e6a991f60d35e9
Min-Uk.Lee <minuk.dev@gmail.com>
2025-09-10 23:05:15 +0900

    feat: define api model of agentgroup

---
+++ api/v1/agentgroup/agentgroup.go
@@ -0,0 +1,32 @@
+// Package agentgroup proviedes the agentgroup API for the server
+package agentgroup
+
+import "github.com/google/uuid"
+
+const (
+	// AgentGroupKind is the kind of the agent group resource.
+	AgentGroupKind = "AgentGroup"
+)
+
+// AgentGroup represents a struct that represents an agent group.
+type AgentGroup struct {
+	UID        uuid.UUID     `json:"uid"`
+	Name       string        `json:"name"`
+	Attributes Attributes    `json:"attributes"`
+	Selector   AgentSelector `json:"selector"`
+	CreatedAt  string        `json:"createdAt"`
+	CreatedBy  string        `json:"createdBy"`
+	DeletedAt  *string       `json:"deletedAt,omitempty"`
+	DeletedBy  *string       `json:"deletedBy,omitempty"`
+} // @name AgentGroup
+
+// Attributes represents a map of attributes for the agent group.
+// @name AgentGroupAttributes
+type Attributes map[string]string
+
+// AgentSelector defines the criteria for selecting agents to be included in the agent group.
+// @name AgentGroupAgentSelector
+type AgentSelector struct {
+	IdentifyingAttributes    map[string]string `json:"identifyingAttributes"`
+	NonIdentifyingAttributes map[string]string `json:"nonIdentifyingAttributes"`
+}
