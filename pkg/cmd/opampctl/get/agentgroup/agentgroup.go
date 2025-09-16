06505ee0a0ceb92987f7b55a6b209860ebb474b4
Min-Uk.Lee <minuk.dev@gmail.com>
2025-09-16 21:47:58 +0900

    wip

---
+++ pkg/cmd/opampctl/get/agentgroup/agentgroup.go
@@ -0,0 +1,178 @@
+// Package agentgroup implements the 'opampctl get agentgroup' command.
+package agentgroup
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+
+	"github.com/google/uuid"
+	"github.com/samber/lo"
+	"github.com/spf13/cobra"
+
+	v1agent "github.com/minuk-dev/opampcommander/api/v1/agent"
+	"github.com/minuk-dev/opampcommander/pkg/client"
+	"github.com/minuk-dev/opampcommander/pkg/clientutil"
+	"github.com/minuk-dev/opampcommander/pkg/formatter"
+	"github.com/minuk-dev/opampcommander/pkg/opampctl/config"
+)
+
+var (
+	// ErrCommandExecutionFailed is returned when the command execution fails.
+	ErrCommandExecutionFailed = errors.New("command execution failed")
+)
+
+// CommandOptions contains the options for the agent command.
+type CommandOptions struct {
+	*config.GlobalConfig
+
+	// flags
+	formatType string
+
+	// internal
+	client *client.Client
+}
+
+// NewCommand creates a new agent command.
+func NewCommand(options CommandOptions) *cobra.Command {
+	//exhaustruct:ignore
+	cmd := &cobra.Command{
+		Use:   "agentgroup",
+		Short: "agentgroup",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			err := options.Prepare(cmd, args)
+			if err != nil {
+				return err
+			}
+
+			err = options.Run(cmd, args)
+			if err != nil {
+				return err
+			}
+
+			return nil
+		},
+	}
+	cmd.Flags().StringVarP(&options.formatType, "output", "o", "short", "Output format (short, text, json, yaml)")
+
+	return cmd
+}
+
+// Prepare prepares the command to run.
+func (opt *CommandOptions) Prepare(*cobra.Command, []string) error {
+	config := opt.GlobalConfig
+
+	client, err := clientutil.NewClient(config)
+	if err != nil {
+		return fmt.Errorf("failed to create authenticated client: %w", err)
+	}
+
+	opt.client = client
+
+	return nil
+}
+
+// Run runs the command.
+func (opt *CommandOptions) Run(cmd *cobra.Command, args []string) error {
+	if len(args) == 0 {
+		err := opt.List(cmd)
+		if err != nil {
+			return fmt.Errorf("list failed: %w", err)
+		}
+
+		return nil
+	}
+
+	agentUIDs := args
+
+	err := opt.Get(cmd, agentUIDs)
+	if err != nil {
+		return fmt.Errorf("get failed: %w", err)
+	}
+
+	return nil
+}
+
+// ShortItemForCLI is a struct that represents an agent item for display.
+type ShortItemForCLI struct {
+	InstanceUID uuid.UUID `short:"Instance UID" text:"instanceUid"`
+}
+
+// List retrieves the list of agents.
+func (opt *CommandOptions) List(cmd *cobra.Command) error {
+	agents, err := clientutil.ListAgentFully(cmd.Context(), opt.client)
+	if err != nil {
+		return fmt.Errorf("failed to list agents: %w", err)
+	}
+
+	switch formatType := formatter.FormatType(opt.formatType); formatType {
+	case formatter.SHORT, formatter.TEXT:
+		displayedAgents := lo.Map(agents, func(agent v1agent.Agent, _ int) ShortItemForCLI {
+			return ShortItemForCLI{
+				InstanceUID: agent.InstanceUID,
+			}
+		})
+		err = formatter.Format(cmd.OutOrStdout(), displayedAgents, formatType)
+	case formatter.JSON, formatter.YAML:
+		err = formatter.Format(cmd.OutOrStdout(), agents, formatType)
+	default:
+		return fmt.Errorf("unsupported format type: %s, %w", opt.formatType, ErrCommandExecutionFailed)
+	}
+
+	if err != nil {
+		return fmt.Errorf("failed to format agents: %w", err)
+	}
+
+	return nil
+}
+
+// Get retrieves the agent information for the given agent UIDs.
+func (opt *CommandOptions) Get(cmd *cobra.Command, names []string) error {
+	type AgentWithErr struct {
+		Agent *v1agent.Agent
+		Err   error
+	}
+
+	agentWithErrs := lo.Map(ids, func(id string, _ int) AgentWithErr {
+		instanceUID, _ := uuid.Parse(id)
+		agent, err := opt.client.AgentService.GetAgent(cmd.Context(), instanceUID)
+
+		return AgentWithErr{
+			Agent: agent,
+			Err:   err,
+		}
+	})
+
+	agents := lo.Filter(agentWithErrs, func(a AgentWithErr, _ int) bool {
+		return a.Err == nil
+	})
+	if len(agents) == 0 {
+		cmd.Println("No agents found or all specified agents could not be retrieved.")
+
+		return nil
+	}
+
+	displayedAgents := lo.Map(agents, func(a AgentWithErr, _ int) ShortItemForCLI {
+		return ShortItemForCLI{
+			InstanceUID: a.Agent.InstanceUID,
+		}
+	})
+
+	err := formatter.Format(cmd.OutOrStdout(), displayedAgents, formatter.FormatType(opt.formatType))
+	if err != nil {
+		return fmt.Errorf("failed to format agents: %w", err)
+	}
+
+	errs := lo.Filter(agentWithErrs, func(a AgentWithErr, _ int) bool {
+		return a.Err != nil
+	})
+	if len(errs) > 0 {
+		errMessages := lo.Map(errs, func(a AgentWithErr, _ int) string {
+			return fmt.Sprintf("failed to get agent %s: %v", a.Agent.InstanceUID, a.Err)
+		})
+
+		cmd.PrintErrf("Some agents could not be retrieved: %s", strings.Join(errMessages, ", "))
+	}
+
+	return nil
+}
