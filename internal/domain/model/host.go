c56753336af709c7e1d361f5e927a6055644d204
Min-Uk.Lee <minuk.dev@gmail.com>
2026-01-21 21:47:37 +0900

    wip

--- internal/domain/model/host.go
+++ internal/domain/model/host.go
@@ -1,4 +1,13 @@
 package model
 
+type HostEnvironment string
+
+const (
+	HostEnvironmentContainer HostEnvironment = "container"
+	HostEnvironmentVM        HostEnvironment = "vm"
+)
+
 // Host is a value object that represents a host.
-type Host struct{}
+type Host struct {
+	Environment HostEnvironment
+}
