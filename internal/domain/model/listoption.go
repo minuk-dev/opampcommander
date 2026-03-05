5405b97782fb838e10575b206baf7a5c5a37ddd4
Min-Uk.Lee <minuk.dev@gmail.com>
2026-03-05 23:10:46 +0900

    feat: add support for including deleted entities in get and list operations

--- internal/domain/model/listoption.go
+++ internal/domain/model/listoption.go
@@ -2,8 +2,14 @@ package model
 
 // ListOptions is a struct that holds options for listing resources.
 type ListOptions struct {
-	Limit    int64
-	Continue string
+	Limit          int64
+	Continue       string
+	IncludeDeleted bool
+}
+
+// GetOptions is a struct that holds options for getting a single resource.
+type GetOptions struct {
+	IncludeDeleted bool
 }
 
 // ListResponse is a generic struct that represents a paginated response for listing resources.
